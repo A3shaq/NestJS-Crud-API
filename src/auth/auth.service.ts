a09d00a2c2f6836c8cb4ea6e50faf01d3910ea99
a3shaq <arshaqshakeel3@gmail.com>
2022-09-15 14:57:42 +0500

    create signup API that saved user in db with unique email

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,51 @@
-import { Injectable } from "@nestjs/common";
-import {User,Bookmark} from "@prisma/client"
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "../prisma/prisma.service";
+import { AuthDto } from "./dto";
+import * as argon from "argon2"
+import { PrismaClientKnownRequestError, PrismaClientValidationError } from "@prisma/client/runtime";
 
 @Injectable()
 export class AuthService {
 
-    constructor(private prisma : PrismaService){
- 
+    constructor(private prisma: PrismaService) {
+
     }
-    signup() {
-        return { msg: "I have Signup" }
+    async signup(dto: AuthDto) {
+        // Generate the passoword hash
+        const hash = await argon.hash(dto.password)
+
+        // Save the new user in the db
+
+        try {
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash
+                }
+
+            })
+
+            delete user.hash
+            return user
+        }
+        catch (error) {
+
+            if (error instanceof PrismaClientKnownRequestError) {
+
+                // P2002 error code satnds for duplicate field error
+                if (error.code === 'P2002') {
+                    throw new ForbiddenException("Email already exist")
+                }
+            }
+
+            throw error;
+        }
+        // we need to return the saved user
+
+        // directy solution remove specfic key
     }
     login() {
-        
+
         return { msg: "I have Login" }
     }
 
