81bcb15c73eaa954da532d0f3d5fb557d8ca5be6
a3shaq <arshaqshakeel3@gmail.com>
2022-09-22 16:19:41 +0500

    fixes

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -77,7 +77,7 @@ export class AuthService {
         }
 
         const token = await this.jwt.signAsync(payload, {
-            expiresIn: '15m',
+            expiresIn: '30m',
             secret: this.config.get('JWT_SECRET')
         })
 
