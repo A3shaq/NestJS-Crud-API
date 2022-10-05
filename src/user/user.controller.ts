901d8632ddfb609aa3e84887402011287e646dfb
a3shaq <arshaqshakeel3@gmail.com>
2022-10-05 16:57:36 +0500

    fix prisma client issue

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,18 @@
-import {Controller,Get,UsePipes,ValidationPipe,UseGuards } from "@nestjs/common";
+import {Controller,Get,UsePipes,ValidationPipe,UseGuards,Req } from "@nestjs/common";
 import {AuthGuard} from "@nestjs/passport"
-
+import {Request} from "express"
 
 @Controller('users')
 @UsePipes(new ValidationPipe({ transform: true, whitelist: true}))
 export class UserController{
     @UseGuards(AuthGuard('jwt'))
     @Get('me')
-    getMe(){
+    getMe(@Req() req:Request){
 
-        return 'User information'
+        return {
+            user:req.user,
+            message:"User Information"
+        }
     }
 
 }
